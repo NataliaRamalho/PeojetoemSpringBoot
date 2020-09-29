b039ba30eda71b745e7d3d18cf58c9c27602a5cf
NataliaRamalho <natalia.ramalho.lopes@gmail.com>
2020-09-29 08:03:07 -0300

    colocando uma rota inicial

---
+++ src/main/java/com/foodComany/sistemaDePedidos/resources/InicioRecursos.java
@@ -0,0 +1,21 @@
+package com.foodComany.sistemaDePedidos.resources;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class InicioRecursos {
+	@GetMapping
+	public ResponseEntity<String> paginaInicial(){
+		 return new ResponseEntity<>("Bem vindo rotas disponiveis: categoria, produtos,pedidos, clientes ", HttpStatus.OK);
+	
+	}
+
+}
+		
+
+
